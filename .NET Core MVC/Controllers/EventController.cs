e81b1fa9b477ff50a97a9a4058b92dd5ab60ec91
kamieniarzk <33696681+kamieniarzk@users.noreply.github.com>
2020-05-06 16:58:41 +0200

    MVC
    .NET Core MVC

---
+++ .NET Core MVC/Controllers/EventController.cs
@@ -0,0 +1,78 @@
+﻿using Calendar.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Calendar.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Delete(string Id)
+        {
+            EventViewModel model = new EventViewModel();
+            string date = model.Get(Id).Date.ToString("yyyy'-'MM'-'dd");
+            if(model.Remove(Id)) 
+            {
+                return Redirect("/Home/Day/?date=" + date);
+            } else
+            {
+                return Redirect("/Shared/Error");
+            }
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.date = HomeController.CurrentDate;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Event/Add/")]
+        public IActionResult Added(string description, string time)
+        {
+            EventViewModel model = new EventViewModel();
+            if (time != null)
+            {
+                time = HomeController.CurrentDate.ToString("yyyy-MM-dd") + 'T' + time;
+                model.Add(new Models.Event() { Date = DateTime.Parse(time), Description = description });
+            }
+            else 
+            {
+                model.Add(new Models.Event() { Date = HomeController.CurrentDate, Description = description });
+            }
+            return Redirect("/Home/Day/?date=" + HomeController.CurrentDate.ToString("yyyy-MM-dd"));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string Id)
+        {
+            EventViewModel model = new EventViewModel();
+            ViewBag.date = HomeController.CurrentDate;
+            ViewBag.Id = Id;
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("/Event/Edit/")]
+        public IActionResult Edit(string description, string id, string time)
+        {
+            EventViewModel model = new EventViewModel();
+            if (time != null)
+            {
+                time = HomeController.CurrentDate.ToString("yyyy-MM-dd") + 'T' + time;
+                model.Edit(id, DateTime.Parse(time), description);
+            }
+            else
+            {
+                model.Edit(id, HomeController.CurrentDate, description);
+            }
+            ViewBag.date = HomeController.CurrentDate;
+            return Redirect("/Home/Day/?date=" + HomeController.CurrentDate.ToString("yyyy-MM-dd"));
+        }
+
+    }
+}
---
+++ .NET Core MVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Calendar.Models;
+using Calendar.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+
+namespace Calendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static DateTime CurrentDate = DateTime.Now;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            EventViewModel events = new EventViewModel();
+            ViewBag.date = CurrentDate.ToString("yyyy-MM-dd");
+            return View(events);
+        }
+
+        public IActionResult Day(string date)
+        {
+            DateTime dt = DateTime.Parse(date);
+            CurrentDate = dt;
+            ViewBag.date = dt;
+            EventViewModel events = new EventViewModel();
+            return View(events);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
